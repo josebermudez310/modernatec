ad32fed5166b6fd229375ba608d0d64871e6faa3
Robin_Rodriguez <rdrb23@gmail.com>
2022-03-17 16:25:00 -0500

    En esta rama y en este commit el back funciona y esta documentado en el archivo modernatec-final

---
+++ back/back_Robin/modernatec-final/resources/views/emails/Citas.blade.php
@@ -0,0 +1,13 @@
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h2>Hola, Se le agendo una cita </h2>
+    <p>El codigo lo debe de colocar en el siguiente enlace para conocer los datalles de la cita. </p>
+    <p>ademas tambien debe de tener en cuenta este codigo para el dia de su cita </p>
+    <p>Este es el codigo de su cita:</p>
+    <h2>{{$code}}</h2>
+</body>
+</html>
+
